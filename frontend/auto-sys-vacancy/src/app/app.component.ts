0683431d9b18a6db45fbc8204b4ce0b2fe0caf9a
Timur <scorpik1992@gmail.com>
2025-02-21 16:51:18 +0600

    make tables ,build components:candidates,vacancies,add ts code

--- frontend/auto-sys-vacancy/src/app/app.component.ts
+++ frontend/auto-sys-vacancy/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {CandidatesComponent }from "./components/candidates/candidates.component"
+import { VacanciesComponent } from "./components/vacancies/vacancies.component"; 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CandidatesComponent, VacanciesComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  selectedTable:string="Candidates"
   title = 'auto-sys-vacancy';
 }
