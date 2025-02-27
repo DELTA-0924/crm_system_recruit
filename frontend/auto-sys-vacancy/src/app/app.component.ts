205dd2c9456eea80351a54f9faa6f4bd337e5815
Tima <scorpik1992@gmail.com>
2025-02-27 19:03:28 +0600

    optimize render components

--- frontend/auto-sys-vacancy/src/app/app.component.ts
+++ frontend/auto-sys-vacancy/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CandidatesComponent }from "./components/candidates/candidates.component"
 import { VacanciesComponent } from "./components/vacancies/vacancies.component"; 
@@ -9,14 +9,39 @@ import {FullCalendarModule} from '@fullcalendar/angular'
 import {CalendarOptions} from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import { CreateCandidateComponent } from './modals/create-candidate/create-candidate.component';
+import { CreateVacancyComponent } from './modals/create-vacancy/create-vacancy.component';
 @Component({
   selector: 'app-root',
-  imports: [CandidatesComponent, VacanciesComponent,CommonModule,CalendarComponent,CreateCandidateComponent],
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   selectedTable:string="Candidates"
   title = 'auto-sys-vacancy';
-
+  selectedComponent!:Type<any>;
+  SelectedModal:Type<any>|null=null
+  ShowComponent(component:string){
+    switch(component){
+      case 'vacancies':
+        this.selectedComponent=VacanciesComponent
+        break;
+      case 'candidates':
+        this.selectedComponent=CandidatesComponent
+        break;
+      case 'calendar':
+        this.selectedComponent=CalendarComponent
+        break;
+    }    
+  }
+  OpenModal(modal:string){
+    switch(modal) {
+      case 'vacancies':
+        this.SelectedModal = (this.SelectedModal === CreateVacancyComponent) ? null : CreateVacancyComponent;
+        break;
+      case 'candidates':
+        this.SelectedModal = (this.SelectedModal === CreateCandidateComponent) ? null : CreateCandidateComponent;
+        break;
+    }
+  }
 }
--- frontend/auto-sys-vacancy/src/main.ts
+++ frontend/auto-sys-vacancy/src/main.ts
@@ -3,8 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { CreateCandidateComponent } from './app/modals/create-candidate/create-candidate.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { CandidatesComponent } from './app/components/candidates/candidates.component';
+import { CalendarComponent } from './app/components/calendar/calendar.component';
+import { VacanciesComponent } from './app/components/vacancies/vacancies.component';
+import { provideRouter } from '@angular/router';
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter([]),
     provideAnimations(),
     ...appConfig.providers // Если в `appConfig` есть другие провайдеры
   ]
